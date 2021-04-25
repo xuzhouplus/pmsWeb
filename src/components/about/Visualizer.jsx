281ee432722f0425fa9e1cd280ddeedef720106c
xuzhou <xuzhou@chinamcloud.com>
2021-04-25 22:25:50 +0800

    修改默认可视化音频

--- src/components/about/Visualizer.jsx
+++ src/components/about/Visualizer.jsx
@@ -8,7 +8,7 @@ class Visualizer extends React.Component {
 		super(props);
 		this.state = {
 			// src: process.env.PUBLIC_URL + '/audio/OneMoreChance.mp3',
-			src: process.env.PUBLIC_URL + '/audio/WhateverItTakes.wav',
+			src: process.env.PUBLIC_URL + '/audio/WhateverItTakes.mp3',
 			type: this.randomType(),//canvas、webgl
 			// src: 'https://m8.music.126.net/21180815163607/04976f67866d4b4d11575ab418904467/ymusic/515a/5508/520b/f0cf47930abbbb0562c9ea61707c4c0b.mp3?infoId=92001',
 			bg1: {
