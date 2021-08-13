fc3958d7391979b1c2b5d708680178f7d99b384d
xuzhou <xuzhou@chinamcloud.com>
2021-08-13 16:32:58 +0800

    修改轮播配置页面样式

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,11 +5,12 @@ import './Footer.scss'
 
 class Footer extends React.PureComponent {
 	render() {
+		const isHomePage = (document.location.pathname === '/' ? true : false)
 		return (
-			<Navbar className="main-color-navbar">
+			<Navbar className={["main-color-navbar", isHomePage ? "home-page" : ""]}>
 				<Nav className="footer-navbar justify-content-center">
 					<Nav.Item>
-						<Nav.Link href="http://beian.miit.gov.cn/" target="_blank" rel="noreferrer noopener">{this.props.site.icp}</Nav.Link>
+						<Nav.Link href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer noopener">{this.props.site.icp}</Nav.Link>
 					</Nav.Item>
 				</Nav>
 			</Navbar>
--- src/components/system/Carousel.jsx
+++ src/components/system/Carousel.jsx
@@ -3,8 +3,8 @@ import {Button, Card, Form} from "react-bootstrap";
 import Utils from "../../utils/Utils";
 import Loading from "../loading/Loading";
 import Swal from "sweetalert2";
-import "./Carousel.scss";
 import {connect} from "react-redux";
+import "./Carousel.scss";
 
 function mapStateToProps(state) {
 	return {
@@ -51,16 +51,37 @@ class Carousel extends React.Component {
 			}
 		}, error => {
 			console.log(error);
-			this.setState({
-				cancelTokenSource: null,
-				settings: {},
-				carousel_type: {},
-				carousel_limit: {},
-				carousel_interval: {}
-			})
+			if(this.state.cancelTokenSource) {
+				this.setState({
+					cancelTokenSource: null,
+					settings: {},
+					carousel_type: {},
+					carousel_limit: {},
+					carousel_interval: {}
+				})
+			}
 		})
 		this.setState({
 			cancelTokenSource: cancelTokenSource
+		})
+	}
+	handleBlur = (event) => {
+		let settings = this.state.settings;
+		settings[event.target.id].value = event.target.value;
+		if (event.target.id === 'carousel_interval') {
+			settings[event.target.id].value = Math.round(settings[event.target.id].value)
+			if (settings[event.target.id].value < 3) {
+				settings[event.target.id].isInvalid = true;
+				settings[event.target.id].isValid = false;
+				settings[event.target.id].text = '不能小于3秒';
+			} else {
+				settings[event.target.id].isInvalid = false;
+				settings[event.target.id].isValid = true;
+				settings[event.target.id].text = '';
+			}
+		}
+		this.setState({
+			settings: settings
 		})
 	}
 	handleChange = (event) => {
@@ -95,14 +116,13 @@ class Carousel extends React.Component {
 			for (let key in options) {
 				items.push(<option key={key} value={key}>{options[key]}</option>);
 			}
-			console.log(this.state);
 			return (
 				<Card className="carousel-settings-container">
 					<Card.Body className="carousel-settings-table">
 						<Form>
 							<Form.Group controlId="carousel_type" className="position-relative">
 								<Form.Label>轮播类型</Form.Label>
-								<Form.Control as="select" onChange={this.handleChange} onBlur={this.handleChange} defaultValue={this.state.settings.carousel_type.value} isInvalid={this.state.carousel_type.isInvalid} isValid={this.state.carousel_type.isValid}>{items}</Form.Control>
+								<Form.Control as="select" onChange={this.handleChange} onBlur={this.handleBlur} defaultValue={this.state.settings.carousel_type.value} isInvalid={this.state.carousel_type.isInvalid} isValid={this.state.carousel_type.isValid}>{items}</Form.Control>
 								<Form.Control.Feedback type="invalid" tooltip>
 									{this.state.carousel_type.text}
 								</Form.Control.Feedback>
@@ -110,15 +130,15 @@ class Carousel extends React.Component {
 							</Form.Group>
 							<Form.Group controlId="carousel_interval" className="position-relative">
 								<Form.Label>轮播间隔时间</Form.Label>
-								<Form.Control onChange={this.handleChange} onBlur={this.handleChange} defaultValue={this.state.settings.carousel_interval.value} isInvalid={this.state.carousel_interval.isInvalid} isValid={this.state.carousel_interval.isValid}></Form.Control>
+								<Form.Control min={3} max={10} type="number" onChange={this.handleChange} onBlur={this.handleBlur} value={this.state.settings.carousel_interval.value} isInvalid={this.state.carousel_interval.isInvalid} isValid={this.state.carousel_interval.isValid}></Form.Control>
 								<Form.Control.Feedback type="invalid" tooltip>
 									{this.state.carousel_interval.text}
 								</Form.Control.Feedback>
 								<Form.Text muted>{this.state.settings.carousel_interval.description}</Form.Text>
 							</Form.Group>
 							<Form.Group controlId="carousel_limit" className="position-relative">
-								<Form.Label>轮播数量限制（{this.state.settings.carousel_limit.value}）</Form.Label>
-								<Form.Control type="range" custom min="3" max="8" onChange={this.handleChange} value={this.state.settings.carousel_limit.value} isInvalid={this.state.carousel_limit.isInvalid} isValid={this.state.carousel_limit.isValid}/>
+								<Form.Label>轮播数量限制</Form.Label>
+								<Form.Control type="number" min="3" max="8" onChange={this.handleChange} value={this.state.settings.carousel_limit.value} isInvalid={this.state.carousel_limit.isInvalid} isValid={this.state.carousel_limit.isValid}/>
 								<Form.Control.Feedback type="invalid" tooltip>
 									{this.state.carousel_limit.text}
 								</Form.Control.Feedback>
